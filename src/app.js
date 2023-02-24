5516f981aed76ca4595a5e2c5e0297b4586b3a3f
Marione Fiszman <marionef1985@gmail.com>
2023-02-24 20:52:29 +0000

    construction of the Project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const numero = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const simbolo = ["❤️", "😉", "😶", "😈"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getRandomNumber = arrlength => Math.floor(Math.random() * arrlength);
+const getRandomItem = arr => arr[getRandomNumber(arr.length)];
+
+window["getCard"] = () => {
+  //declararcarta inicial
+  //const card = { symbol: null, value: null };
+  const randomNumber = getRandomItem(numero);
+  const randomSymbolUp = getRandomItem(simbolo);
+  const randomSymbolDown = randomSymbolUp;
+  const numToShow = `${randomNumber}`;
+  const symbDownToShow = `${randomSymbolDown}`;
+  const symbUpToShow = `${randomSymbolUp}`;
+  document.querySelector("#number").innerHTML = numToShow;
+  document.querySelector("#upSymbol").innerHTML = symbUpToShow;
+  document.querySelector("#downSymbol").innerHTML = symbDownToShow;
+  //  Intentar:
+  // const numToShow = `${getRandomItem(numero)}`;
 };
